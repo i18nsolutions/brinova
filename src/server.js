5bf4faba40831d90b07926552548e3f716abecb0
i18nsolutions <househopers@gmail.com>
2018-01-30 20:55:13 +0530

    express-favicon

--- src/server.js
+++ src/server.js
@@ -7,3 +7,16 @@ app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+const express = require('express');
+const favicon = require('express-favicon');
+ 
+const app = express();
+ 
+app.use(favicon(__dirname + './favicon.png'));
+ 
+// Add your routes here, etc. 
+ 
+const server = app.listen(3000, function(){
+    console.log('server is running at %s .', server.address().port);
+});
