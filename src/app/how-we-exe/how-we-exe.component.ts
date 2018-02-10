f52ad0d190ec87dab8b2d994024205c447d42a53
Sathish <psathishsivam@gmail.com>
2018-02-10 14:44:19 +0530

    final changes 10-02-18

--- src/app/how-we-exe/how-we-exe.component.ts
+++ src/app/how-we-exe/how-we-exe.component.ts
@@ -15,7 +15,7 @@ export class HowWeExeComponent implements OnInit {
                   "Define Smaller Milestones","Define Complete Requirements","Define System Architecture",
                   "Optimize Design"];
   private BfList4=["Effective Code Implementation","Rigourous Testing and Validation",
-  "Documentation of Requirement and Design aspects","Iterative Reviews","Project Control Management",
+  "Documentation of Requirement & Design aspects","Iterative Reviews","Project Control Management",
   "Quality Control","User Acceptance and Deployment"];
 
 }
